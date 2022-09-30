24809954c22fad83b4436e25f5fdc4b27c3a51aa
wakoliVotes <votesdw@yahoo.com>
2022-09-30 09:50:30 +0300

    StalkingSkies first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import SearchMain from './components/SearchMain';
+
+function App() {
+  return (
+    <div className='App'>
+     <SearchMain />
+    </div>
+  );
+}
+
+export default App;
